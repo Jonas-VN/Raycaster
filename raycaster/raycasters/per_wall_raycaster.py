a2273caa3262d2672f281635106e56ee205079c4
Jonas Van Nieuwenhuyze <87768347+Jonas-VN@users.noreply.github.com>
2023-06-24 12:03:14 +0200

    fix bug in per_wall

--- raycaster/raycasters/per_wall_raycaster.py
+++ raycaster/raycasters/per_wall_raycaster.py
@@ -14,7 +14,8 @@ class PerWallRaycaster(Raycaster):
         prev_ray = self.rays[0]
         for ray in self.rays:
             # different coordinate and not lie in line with each other
-            if start_ray.map_coordinate[ray.hit_direction] != ray.map_coordinate[ray.hit_direction]:
+            if start_ray.map_coordinate[start_ray.hit_direction] != ray.map_coordinate[start_ray.hit_direction] or \
+                    start_ray.map_coordinate[ray.hit_direction] != ray.map_coordinate[ray.hit_direction]:
                 # always render with the previous ray because the current ray failed the condition, so it's not the same wall anymore
                 self.__render_wall_segment(start_ray, prev_ray)
                 start_ray = prev_ray
@@ -32,6 +33,8 @@ class PerWallRaycaster(Raycaster):
         self.renderer.update_screen()
 
     def __render_wall_segment(self, start_ray, end_ray):
+        # print(
+        #     f"Start: {start_ray.map_coordinate}, End: {end_ray.map_coordinate}")
         start_points = start_ray.get_line()
         end_points = end_ray.get_line()
         c = 255 - 100 * start_ray.hit_direction
